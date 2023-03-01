5e5e92038f8d3b0f433d12731b25dfddc598ad27
mohamed madobe <mohamedmadobe@mohameds-MacBook-Pro.local>
2023-03-01 13:29:33 -0600

    oop python exercise

---
+++ opp_exercise.py
@@ -0,0 +1,27 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+    def __init__(self, start = 0):
+        self.start = self.next = start
+        def __init__(self):
+                 return f"<SerialGenerator start={self.start} next={self.next}>"
+        def reset (reset):
+              self.next =self>start
